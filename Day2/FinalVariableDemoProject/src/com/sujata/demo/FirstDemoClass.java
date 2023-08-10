506097f0e6f855f87c4c3f716531eb83d1c1ddd6
Sujatabatra <sujatabatra@hotmail.com>
2023-08-10 15:10:47 +0530

    Add files via upload

---
+++ Day2/FinalVariableDemoProject/src/com/sujata/demo/FirstDemoClass.java
@@ -0,0 +1,22 @@
+package com.sujata.demo;
+
+public class FirstDemoClass {
+    final int x;
+
+    public FirstDemoClass(int x) {
+        this.x = x;
+    }
+
+    public void demoMethod(){
+//        x=10;
+        final int y;
+        y=10;
+//        y=20;
+
+    }
+
+    public void print(final int a){
+        
+    }
+
+}
