76f0cd297f74bd1878e4252ca0e43e41237e285f
Sujatabatra <sujatabatra@hotmail.com>
2023-08-18 11:26:52 +0530

    Add files via upload

---
+++ Day7/MyMultiThreadingDemoProject/src/com/sujata/demo/MyFirstDemoClass.java
@@ -0,0 +1,30 @@
+package com.sujata.demo;
+
+class FirstThread extends Thread{
+    @Override
+    public void run(){
+        for(int i=1;i<=5;i++) {
+            System.out.println("Welcome to " + Thread.currentThread().getName()+" i : "+i);
+        }
+    }
+}
+public class MyFirstDemoClass {
+    public static void main(String args[]){
+    //New Born state
+        FirstThread childThread=new FirstThread();
+
+        childThread.start();  //Ready or Running
+
+
+        for(int i=1;i<=5;i++) {
+            System.out.println("Welcome to " + Thread.currentThread().getName()+" i : "+i);
+            try{
+                Thread.sleep(500);
+            }
+            catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("Good Bye from "+Thread.currentThread().getName());
+    }
+}
