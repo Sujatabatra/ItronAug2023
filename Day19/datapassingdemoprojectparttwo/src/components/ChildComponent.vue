073436acd3429477d18ed3b286996c4508e35b1c
Sujatabatra <sujatabatra@hotmail.com>
2023-09-05 15:59:08 +0530

    Add files via upload

---
+++ Day19/datapassingdemoprojectparttwo/src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Hi I am Child Component</h1>
+    <input type="text" v-model="userName" placeholder="Enter User Name"><br>
+    <button v-on:click="getUser(userName)">Send User Name To Parent Compoenent</button>
+</template>
+<script>
+export default{
+    name:"ChildComponent",
+    props:{
+        getUser:Function
+    },
+    data(){
+        return{
+            userName:"Sujata"
+        }
+    }
+}
+</script>
---
+++ Day19/datapassingdemoprojectparttwo/src/components/ParentsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Hi I am Parents Component</h1>
+    <h2>User Name from Child : {{ childName }}</h2>
+    <hr>
+    <ChildComponent :getUser="getUserName"></ChildComponent>
+</template>
+<script>
+import ChildComponent
+ from './ChildComponent.vue';
+export default{
+    name:"ParentComponent",
+    components:{
+        ChildComponent
+    },
+    data(){
+        return{
+            childName:''
+        }
+    },
+    methods:{
+        getUserName(name){
+            this.childName=name;
+        }
+    }
+}
+</script>
