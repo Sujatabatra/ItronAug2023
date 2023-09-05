baeb71258e4c41095257f649964d166b12b29329
Sujatabatra <sujatabatra@hotmail.com>
2023-09-05 11:26:59 +0530

    Add files via upload

---
+++ Day19/eventdemoproject/src/components/EventComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Event Handling Component</h1>
+    <h2>Counter : {{ count }}</h2>
+    <button v-on:click="getCount" >Increment Counter</button><br><br>
+    <button v-on:click="getSum">Calculate Sum</button>
+    <h2>{{ sum }}</h2>
+</template>
+<script>
+export default{
+    name:"EventComponent",
+    data(){
+        return{
+            count:0,
+            sum:''
+        }
+    },
+    methods:{
+       getCount(){
+            this.count++;
+       } ,
+       getSum(){
+        var value1=prompt("Enter First Number : ",0);
+        var value2=prompt("Enter Second Number : ",0);
+        this.sum=parseInt(value1)+parseInt(value2);
+       }
+    }
+}
+</script>
---
+++ Day19/interpolationdemoproject/src/components/FirstComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Hi I am First Component</h1>
+        <h2>Name : {{ name }}</h2>
+        <h2>Email : {{email}}</h2>
+        <h3>{{ greet("Sujata Batra") }}</h3>
+        <h1>Calling Methods</h1>
+        <h2>{{ addSalutation("Arindam") }}</h2>
+        <h2>{{ getData() }}</h2>
+    </div>
+</template>
+<script>
+export default{
+    name:'FirstComponent',
+    data(){
+        return{
+        name : 'Sujata',
+        email:'sujatabatra@hotmail.com',
+        greet: function(na){
+            return "Welcome to Vue JS "+this.name+na
+        }
+        }
+    },
+    methods:{
+        addSalutation(name){
+            return "Mr./Ms. "+name;
+        },
+        getData(){
+            return{
+                name : this.name,
+                mailId:this.email
+            } 
+        }
+        
+    }
+
+}
+</script>
+<style>
+    div{
+        color: orange;
+    }
+</style>
