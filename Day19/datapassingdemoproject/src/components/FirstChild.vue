b06d3b6fd46eb6086ea3933c26c297f5138244e6
Sujatabatra <sujatabatra@hotmail.com>
2023-09-05 15:29:20 +0530

    Add files via upload

---
+++ Day19/datapassingdemoproject/src/components/FirstChild.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- <h1>Hi I am First Child Component</h1>
+        <h2>{{ message }}</h2>
+        <h2>{{ user }}</h2>
+        <button v-on:click="getUserData">Click Me</button> -->
+        <p>{{ data.firstName }} {{ data.lastName }}</p>
+    </div>
+</template>
+<script>
+    export default{
+        name:"FirstChild",
+        props:{
+            message:String,
+            user:Object,
+            getUserData:Function,
+            data:Object
+        }
+    }
+</script>
+<!-- <style scoped>
+    div{
+        background-color: coral;
+        color:aqua;
+        height:300px;
+        width: 400px;
+        margin: 10px;
+        padding: 10px;;
+    }
+</style> -->
---
+++ Day19/datapassingdemoproject/src/components/ParentsComponent.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <h1>Hi I am Parent Component</h1>
+    <!-- <FirstChild message="Hello from Parent to Child Component" :user="user" :getUserData="getData"></FirstChild> -->
+    <hr>
+    <ul>
+        <li v-for="user in users" :key="user.firstName">
+            <FirstChild :data="user"></FirstChild>
+        </li>
+    </ul>
+</div>
+</template>
+<script>
+import FirstChild from './FirstChild.vue';
+export default{
+    name:"ParentComponent",
+    components:{
+        FirstChild
+    },
+    data(){
+        return{
+            user:{firstName:"Sujata",lastName:"Batra"},
+            users:[{firstName:"Sujata",lastName:"Batra"},
+            {firstName:"Arindam",lastName:"Singh"},
+            {firstName:"Amrit",lastName:"Raj"},
+            {firstName:"Krithik",lastName:"Gopinath"},
+            {firstName:"Prateek",lastName:"Kushwaha"}]
+        }
+    },
+    methods:{
+        getData(){
+            console.log(this.user);
+            alert(this.user.firstName+" "+this.user.lastName);
+        }
+    }
+
+}
+</script>
+<!-- <style scoped>
+    div{
+        background-color: gray;
+        height: 400px;
+
+    }
+</style> -->
